c1f0f8087a9748a8f09c30b17082859b3229d7d1
luofan <luofanhs6666@163.com>
2019-07-11 22:20:20 +0800

    用户管理完成

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -7,9 +7,9 @@
     <el-breadcrumb-item>用户列表</el-breadcrumb-item>
   </el-breadcrumb>
   <!-- 搜索框 -->
-  <div style="margin-top: 15px;">
+  <div>
     <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
-      <el-button slot="append" icon="el-icon-search"></el-button>
+      <el-button slot="append" icon="el-icon-search" @click="queryUser"></el-button>
     </el-input>
   </div>
   <!-- 表格 -->
@@ -33,29 +33,125 @@
     </el-table-column>
     <el-table-column
       label="用户状态">
+      <!--
+        v-slot:default="{ row }" 指作用域插槽
+        default是默认插槽的意思
+       -->
+      <template v-slot:default="{ row }">
+        <el-switch
+          v-model="row.mg_state"
+          active-color="#13ce66"
+          inactive-color="#ff4949">
+        </el-switch>
+      </template>
     </el-table-column>
     <el-table-column
       label="操作">
+      <template v-slot:default="{ row }">
+        <el-button type="primary" icon="el-icon-edit" plain round></el-button>
+        <el-button type="danger" icon="el-icon-delete" plain round @click="deleteUser(row.id)"></el-button>
+        <el-button type="success" icon="el-icon-check" plain round>分配角色</el-button>
+      </template>
     </el-table-column>
   </el-table>
+  <!-- 分页 -->
+  <el-pagination
+    background
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    :current-page="pagenum"
+    :page-sizes="[2, 4, 6, 8, 10]"
+    :page-size="pagesize"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="total">
+  </el-pagination>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       query: '',
-      tabledata: ''
+      tableData: [],
+      pagenum: 1,
+      pagesize: 2,
+      total: 0
     }
   },
+  // 钩子函数
   created () {
     this.getUserList()
+    console.log(this)
+  },
+  methods: {
+    getUserList () {
+      axios.get('http://localhost:8888/api/private/v1/users', {
+        params: {
+          query: this.query,
+          pagenum: this.pagenum,
+          pagesize: this.pagesize
+        },
+        // 设置请求头
+        // 要渲染列表要加上token
+        // headers 请求头
+        // Authorization 授权
+        headers: {
+          Authorization: localStorage.getItem('token')
+        }
+      }).then(res => {
+        const { meta, data } = res.data
+        if (meta.status === 200) {
+          this.tableData = data.users
+          this.total = data.total
+        }
+      })
+    },
+    handleSizeChange (val) {
+      this.pagesize = val
+      this.pagenum = 1
+      this.getUserList()
+    },
+    handleCurrentChange (val) {
+      this.pagenum = val
+      this.getUserList()
+    },
+    queryUser () {
+      this.pagenum = 1
+      this.getUserList()
+    },
+    deleteUser (id) {
+      console.log(id)
+      this.$confirm('确定删除吗?', '温馨提示', {
+        type: 'warning'
+      }).then(() => [
+        axios.delete(`http://localhost:8888/api/private/v1/users/${id}`, {
+          headers: {
+            Authorization: localStorage.getItem('token')
+          }
+        }).then(res => {
+          const { meta } = res.data
+          console.log(res)
+          if (meta.status === 200) {
+            this.$message.success('删除成功')
+            // 判断，如果当前页就剩下一条，应该让pagenum -1
+            if (this.tableData.length === 1 && this.pagenum > 1) {
+              this.pagenum--
+            }
+            // 重新渲染
+            this.getUserList()
+          } else {
+            this.$message.error(meta.msg)
+          }
+        })
+      ])
+    }
   }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
 .el-breadcrumb{
   height: 40px;
   line-height: 40px;
--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import ElementUI from 'element-ui'
 // 引入elementUI的样式
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+// import axios from 'axios'
+// // 把axios放在vue的原型上
+// Vue.prototype.axios = axios
+// // 设置axios全集的baseURL,只要设置了全局的base_url，后面的请求会自动拼接上base_url
+// axios.defaults.baseUrl = 'http://localhost:8888/api/private/v1/'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
