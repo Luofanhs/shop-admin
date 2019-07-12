d1878f419e5ef5e850557204c80e15f4513fe8e9
luofan <luofanhs6666@163.com>
2019-07-12 18:16:43 +0800

    分类页面完成

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -11,6 +11,7 @@
     <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
       <el-button slot="append" icon="el-icon-search" @click="queryUser"></el-button>
     </el-input>
+    <el-button type="success" plain style="margin-left:20px">添加用户</el-button>
   </div>
   <!-- 表格 -->
   <el-table
@@ -32,11 +33,7 @@
       label="电话">
     </el-table-column>
     <el-table-column
-      label="用户状态">
-      <!--
-        v-slot:default="{ row }" 指作用域插槽
-        default是默认插槽的意思
-       -->
+      label="状态">
       <template v-slot:default="{ row }">
         <el-switch
           v-model="row.mg_state"
@@ -44,27 +41,33 @@
           inactive-color="#ff4949">
         </el-switch>
       </template>
+
     </el-table-column>
     <el-table-column
       label="操作">
-      <template v-slot:default="{ row }">
-        <el-button type="primary" icon="el-icon-edit" plain round></el-button>
-        <el-button type="danger" icon="el-icon-delete" plain round @click="deleteUser(row.id)"></el-button>
-        <el-button type="success" icon="el-icon-check" plain round>分配角色</el-button>
+      <template v-slot:default="{row}">
+        <el-button type="primary" icon="el-icon-edit" plain size="small"></el-button>
+        <el-button type="danger" icon="el-icon-delete" @click="deleteUser(row.id)" plain size="small"></el-button>
+        <el-button type="success" icon="el-icon-check" plain size="small">分配角色</el-button>
       </template>
     </el-table-column>
   </el-table>
   <!-- 分页 -->
-  <el-pagination
-    background
-    @size-change="handleSizeChange"
-    @current-change="handleCurrentChange"
-    :current-page="pagenum"
-    :page-sizes="[2, 4, 6, 8, 10]"
-    :page-size="pagesize"
-    layout="total, sizes, prev, pager, next, jumper"
-    :total="total">
-  </el-pagination>
+  <div class="block">
+    <!--
+      size-change 指下拉菜单变化
+      current-change 指当前页发生变化
+     -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2, 4, 6]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+  </div>
 </div>
 </template>
 
@@ -80,28 +83,26 @@ export default {
       total: 0
     }
   },
-  // 钩子函数
+  // 一进页面就渲染
   created () {
-    this.getUserList()
-    console.log(this)
+    this.getrequest()
   },
   methods: {
-    getUserList () {
-      axios.get('http://localhost:8888/api/private/v1/users', {
+    // 封装ajax
+    getrequest () {
+      axios({
+        url: 'http://localhost:8888/api/private/v1/users',
         params: {
           query: this.query,
           pagenum: this.pagenum,
           pagesize: this.pagesize
         },
-        // 设置请求头
-        // 要渲染列表要加上token
-        // headers 请求头
-        // Authorization 授权
         headers: {
           Authorization: localStorage.getItem('token')
         }
       }).then(res => {
-        const { meta, data } = res.data
+        console.log(res.data)
+        const { data, meta } = res.data
         if (meta.status === 200) {
           this.tableData = data.users
           this.total = data.total
@@ -109,43 +110,43 @@ export default {
       })
     },
     handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
       this.pagesize = val
       this.pagenum = 1
-      this.getUserList()
+      this.getrequest()
     },
     handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
       this.pagenum = val
-      this.getUserList()
+      this.getrequest()
     },
     queryUser () {
       this.pagenum = 1
-      this.getUserList()
+      this.getrequest()
     },
+    // 删除
     deleteUser (id) {
-      console.log(id)
       this.$confirm('确定删除吗?', '温馨提示', {
         type: 'warning'
-      }).then(() => [
-        axios.delete(`http://localhost:8888/api/private/v1/users/${id}`, {
+      }).then(() => {
+        axios({
+          method: 'delete',
+          url: `http://localhost:8888/api/private/v1/users/${id}`,
           headers: {
             Authorization: localStorage.getItem('token')
           }
         }).then(res => {
+          console.log(res.data)
           const { meta } = res.data
-          console.log(res)
           if (meta.status === 200) {
             this.$message.success('删除成功')
-            // 判断，如果当前页就剩下一条，应该让pagenum -1
             if (this.tableData.length === 1 && this.pagenum > 1) {
               this.pagenum--
             }
-            // 重新渲染
-            this.getUserList()
-          } else {
-            this.$message.error(meta.msg)
+            this.getrequest()
           }
         })
-      ])
+      })
     }
   }
 }
